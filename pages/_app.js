39871625e767ecaef56927e357682e24d585e787
Raghav <aroraraghav008@gmail.com>
2022-03-28 09:51:59 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import "../Components/form.css"
+import "../Components/cards.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
